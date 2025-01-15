53231ce6cf68bf44c1a78c797a046db23558ee56
DrDeath <kungfuverde@gmail.com>
2025-01-15 09:40:40 +0100

    Added energy potion funcionality

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/runecrafting/runebodycrafter/RuneBodyCrafterConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/runecrafting/runebodycrafter/RuneBodyCrafterConfig.java
@@ -25,4 +25,24 @@ public interface RuneBodyCrafterConfig extends Config {
     default String about() {
         return "This plugin crafts runes at the body altar.\n\nOnly works with Pure essence.\n\nBe near the Edgeville bank before starting the script and have a Body Tiara in the Bank.\n\nThank you to George and Harvest101 for the help!";
     }
+
+    @ConfigSection(
+            name = "Use energy potions",
+            description = "",
+            position = 1
+    )
+    String energypotionSection = "Energy potion";
+
+    @ConfigItem(
+            keyName = "Energypotion",
+            name = "Use energy potions",
+            description = "",
+            position = 1,
+            section = energypotionSection
+    )
+    default boolean sEnergypotion() {
+        return false;
+    }
+
+
 }
