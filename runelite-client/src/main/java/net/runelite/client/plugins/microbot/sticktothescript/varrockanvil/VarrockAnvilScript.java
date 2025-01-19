ef9d3949678bcc134fecf7d469fc08ade62c333e
DrDeath <kungfuverde@gmail.com>
2025-01-19 08:10:47 +0100

    Increasing waiting time for expectingXPDrop, it's not enough sometimes.

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/sticktothescript/varrockanvil/VarrockAnvilScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/sticktothescript/varrockanvil/VarrockAnvilScript.java
@@ -90,7 +90,7 @@ public class VarrockAnvilScript extends Script {
                         return;
                     }
 
-                    if (expectingXPDrop && Rs2Player.waitForXpDrop(Skill.SMITHING, 20000)) {
+                    if (expectingXPDrop && Rs2Player.waitForXpDrop(Skill.SMITHING, 10000)) {
                         debug("Smithing in progress");
                         Rs2Antiban.actionCooldown();
                         Rs2Antiban.takeMicroBreakByChance();
