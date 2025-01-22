9fb79a67f50264ae6be556c739e26f292df0a1db
DrDeath <kungfuverde@gmail.com>
2025-01-22 19:17:34 +0100

    Missing a space in plugin description

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/roguesden/RoguesDenConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/roguesden/RoguesDenConfig.java
@@ -5,6 +5,6 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigInformation;
 
 @ConfigGroup("RoguesDen")
-@ConfigInformation("This plugin requires atleast 80 thieving. Start at the rogues den.")
+@ConfigInformation("This plugin requires at least 80 thieving. Start at the rogues den.")
 public interface RoguesDenConfig extends Config {
 }
